abd9f27488f0ddfb7efc56f124860faade4ce11e
adi960280-cmd <adityamalhotra960@gmail.com>
2025-12-31 19:50:22 +0530

    Update vars.py with new BOT_TOKEN and formatting

--- modules/vars.py
+++ modules/vars.py
@@ -5,7 +5,7 @@ from os import environ
 
 API_ID = int(environ.get("API_ID", "29115102"))
 API_HASH = environ.get("API_HASH", "1a331db2b00e9d2decaa9c7276449eb6")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
+BOT_TOKEN = environ.get("BOT_TOKEN", "7998257759:AAFGOPn-12Mkjtz4uTbuEDNMkM9jNRRjK9c")
 
 OWNER = int(environ.get("OWNER", "1224092270"))
 CREDIT = environ.get("CREDIT", "𝙎𝘼𝙄𝙉𝙄 𝘽𝙊𝙏𝙎")
@@ -18,4 +18,5 @@ AUTH_USER = os.environ.get('AUTH_USERS', '1224092270').split(',')
 AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
 if int(OWNER) not in AUTH_USERS:
     AUTH_USERS.append(int(OWNER))
+
 
